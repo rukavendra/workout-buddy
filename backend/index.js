2050b9d44a6dc22cd07725e008c4f427f4c19c2e
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-10-09 20:21:16 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,9 @@ const app = express()
 
 // }))
 
-app.use(cors())
+app.use(cors({origin: '*'}))
 
-app.use(express.json({origin: '*'}))
+app.use(express.json())
 
 // app.use('/', (req,res)=>res.send("Welcome to Workout Buddy"))
 
