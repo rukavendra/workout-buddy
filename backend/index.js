5f8898a0836ee35bb1bc1f8cc608484aef066edd
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-10-09 23:36:27 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,13 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 app.use(express.json());
 
 app.use((req, res, next) => {
